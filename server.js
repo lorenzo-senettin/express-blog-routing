fb5314a06829e113aec363909aeb4a5577f13668
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-03-13 20:14:21 +0100

    Add route for (/), import post router and use

--- server.js
+++ server.js
@@ -6,3 +6,12 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+// Set up route for root URL ("/")
+app.get("/", (req, res) => {
+  res.send("Welcome to our server!");
+});
+
+// Import Post Router and use it in server.js
+const postRouter = require("./routers/post");
+app.use("/posts", postRouter);
